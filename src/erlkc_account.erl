29d50731cff93cbd77a0f2a9ef614503b8a7e484
ctxhaard <c.tomasin@gmail.com>
2021-01-22 17:12:07 +0100

    a big jump forward

--- src/erlkc_account.erl
+++ src/erlkc_account.erl
@@ -1,7 +1,7 @@
 -module(erlkc_account).
 -author('c.tomasin@gmail.com').
 
--export([load/2, main/1]).
+-export([load/2, matches/2, main/1]).
 
 
 load(_FilePath, Pwd) ->
@@ -11,36 +11,39 @@ load(_FilePath, Pwd) ->
     % che la password non sia visibile nella lista dei processi
     Port = open_port(
         {spawn, <<"openssl enc -d -aes-256-cbc -md sha256 -in archive.protected">>},
-        [ use_stdio, exit_status, {line, 255} ]
+        [ use_stdio, stderr_to_stdout, exit_status, {line, 255} ]
     ),
     decode(Port, Pwd).
 
 decode(Port, Pwd) ->
     Port ! {self(), {command, [Pwd, "\n"] }},
-    loop(Port, []).
+    receive_loop(Port, []).
 
-loop(Port, Accumulator) ->
+receive_loop(Port, Accumulator) ->
     receive
         {Port , {data, {eol, Line}}} ->
             case iolist_to_binary(Line) of
                 <<"---">> ->
-                    loop(Port, [{account, #{}}|Accumulator]); % new data structure
+                    receive_loop(Port, [{account, #{}}|Accumulator]); % new data structure
                 <<"t: ", Value/binary>> ->
-                    loop(Port, add_field(Accumulator, title, Value));
+                    receive_loop(Port, add_field(Accumulator, title, Value));
                 <<"url: ", Value/binary>> ->
-                    loop(Port, add_field(Accumulator, url, Value));
+                    receive_loop(Port, add_field(Accumulator, url, Value));
                 % username
                 <<"u: ", Value/binary>> ->
-                    loop(Port, add_field(Accumulator, username, Value));
+                    receive_loop(Port, add_field(Accumulator, username, Value));
                 % password
                 <<"p: ", Value/binary>> ->
-                    loop(Port, add_field(Accumulator, password, Value));
+                    receive_loop(Port, add_field(Accumulator, password, Value));
                 % notes
                 <<"n: ", Value/binary>> ->
-                    loop(Port, add_field(Accumulator, notes, Value));
+                    receive_loop(Port, add_field(Accumulator, notes, Value));
                 % other
                 <<"o: ", Value/binary>> ->
-                    loop(Port, add_field(Accumulator, other, Value))
+                    receive_loop(Port, add_field(Accumulator, other, Value));
+                % discard unmanaged lines
+                _ ->
+                    receive_loop(Port, Accumulator)
             end;
         {Port, {exit_status, 0}} ->
             lists:reverse(Accumulator);
@@ -48,29 +51,25 @@ loop(Port, Accumulator) ->
             exit(1)
     end.
 
-    % [ 
-    %     {account, [
-    %         {title, "This is the title"}, 
-    %         {url, "http://blah.org"}, 
-    %         {username, "mario.rossi"}, 
-    %         {password, "1234567"}, 
-    %         {note, "Test account"}, 
-    %         {others, "Dunno what this field means"}]},
-
-    %     {account, [
-    %         {title, "Yet another title"}, 
-    %         {url, "http://youp.org"}, 
-    %         {username, "andrea.bianchi"}, 
-    %         {password, "abcdef"}, 
-    %         {note, "Test account #2"}, 
-    %         {others, "Dunno what this field means #2"}]} 
-
-    % ].
-
 add_field(Accumulator, Field, Value) ->
     [{account, Map} | Tail] = Accumulator,
     [{account, maps:put(Field, Value, Map) } | Tail].
 
+matches({account, Map}, MP) ->
+    First = maps:iterator(Map),
+    Loop = fun(F,I) ->
+        case maps:next(I) of
+            none -> false;
+            {_K, V, Next} -> 
+                case re:run(V, MP) of
+                    {match, _} -> true;
+                    _ -> F(F, Next)
+                end
+        end
+    end,
+    Loop(Loop,First).
+
+% called by ERTS when run as a script
 main(Args) ->
     [FileName|Password] = Args,
     Accounts = load(FileName, Password),
--- src/erlkeychain.erl
+++ src/erlkeychain.erl
@@ -6,7 +6,7 @@
 
 -define(ARCHIVEPATH, "archive.protected").
 
--import_lib("cecho/include/cecho.erl").
+-include_lib("cecho/include/cecho.hrl").
 %%====================================================================
 %% API functions
 %%====================================================================
@@ -15,15 +15,21 @@
 main(_Args) ->
     ok = application:start(cecho),
     ok = cecho:cbreak(),
+    % disable echo to hide user password
     ok = cecho:noecho(),
 
     {MaxY, MaxX} = cecho:getmaxyx(),
-    Win = cecho:newwin(MaxY, MaxX, MaxY - 2, 0),
+    Win = cecho:newwin(MaxY -2, MaxX, 0, 0),
+    WinP = cecho:newwin(2, MaxX, MaxY-2, 0),
 
-    cecho:waddstr(Win,"Insert your password: "),
-    cecho:wrefresh(Win),
-    Accounts = erlkc_account:load(?ARCHIVEPATH, get_password()),
-    accounts_summary(Accounts),
+    Prompt = make_prompt(WinP, input),
+    Password = make_prompt(WinP, password),
+
+    Pwd = Password("Insert your password"),
+
+    Accounts = erlkc_account:load(?ARCHIVEPATH, Pwd),
+
+    loop(all, Accounts, Win, Prompt, Pwd),
 
     % TEMP: to avoid program to shut down immediately
     cecho:getch(),
@@ -32,17 +38,74 @@ main(_Args) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-get_password() ->
-    get_password([]).
+make_prompt(Win, Type) ->
+    fun(Text) ->
+        cecho:werase(Win),
+        cecho:mvwaddstr(Win, 0, 0,[Text, ": "]),
+        cecho:wrefresh(Win),
+        case Type of
+            password -> getstr(password);
+            _ -> getstr(Win)
+        end
+    end.
 
-get_password(Tail) ->
+getstr(Win) ->
+    getstr(Win,[]).
+
+getstr(Win,Tail) ->
     case cecho:getch() of
         $\n -> 
             lists:reverse(Tail);
-        Char -> 
-            get_password([Char|Tail])
+        Char ->
+            if Win =/= password -> 
+                cecho:waddstr(Win, [Char]),
+                cecho:wrefresh(Win);
+                true -> ok
+            end,
+            getstr(Win, [Char|Tail])
     end.
 
-accounts_summary(_Accounts) ->
-    % TODO: print accounts summary
-    ok.
+accounts_summary(Win, _N, []) ->
+    cecho:wrefresh(Win);
+accounts_summary(Win, N, Accounts) ->
+    case cecho:getmaxyx(Win) of
+        {MaxY, _} when N > MaxY ->
+            cecho:wrefresh(Win);
+        _ ->
+            [A|Tail] = Accounts,
+            {account, #{ title := Title }} = A,
+            cecho:mvwaddstr(Win, N, 0, io_lib:format("~B: ~s", [N + 1, Title])),
+            accounts_summary(Win, N+1, Tail)
+    end.
+
+loop(all, Accounts, Win, Prompt, Pwd) ->
+    accounts_summary(Win, 0, Accounts),
+    Text = Prompt( io_lib:format("Select an account by keyword or by index (1-~B 0:add new [Q]uit)", [length(Accounts)]) ),
+    loop(getcommand(Text), Accounts, Win, Prompt, Pwd);
+loop(quit, _, _, _, _) ->
+    ok;
+loop(addnew, Accounts, Win, Prompt, Pwd) ->
+    ok;
+loop(Command, Accounts, Win, Prompt, Pwd) when is_integer(Command) ->
+    % stampare l'account selezionato
+    ok;
+loop(Regex, Accounts, Win, Prompt, Pwd) ->
+    FilteredAccounts =  case re:compile(Regex, [caseless]) of
+                            {ok, MP} -> [ X || X <- Accounts, erlkc_account:matches(X, MP) ];
+                            _ -> Accounts
+                        end,
+    accounts_summary(Win, 0, FilteredAccounts),
+    Text = Prompt( io_lib:format("Select an account by keyword or by index (1-~B 0:add new [Q]uit)", [length(Accounts)]) ),
+    loop(getcommand(Text), Accounts, Win, Prompt, Pwd).
+
+getcommand([]) ->
+    all;
+getcommand(Text) when Text == "q" orelse Text == "Q" ->
+    quit;
+getcommand(Text) when Text == "0" ->
+    addnew;
+getcommand(Text) ->
+    case string:to_integer(Text) of
+        {Num, []} -> Num;
+        _ -> Text
+    end.
